f8b50047105c249d2348767462522ce5065bdfd9
RGBPlaza <rayred14@live.com>
2016-10-09 22:21:28 +0100

    step 1 blur

--- RGBSlide/rgbSlide.js
+++ RGBSlide/rgbSlide.js
@@ -8,7 +8,7 @@
         });
 
         slideInterval = slideInterval || 3300;
-        $("head").prepend('<style>.sliderBox{background-color:#222;z-index:-1;margin:0;position:absolute;left:0;top:0;width:100%;height:80vh;}'
+        $("head").prepend('<style>.sliderBox{background-color:#222;z-index:-1;margin:0;position:absolute;left:0;top:0;width:100%;height:80vh;transition:filter,-webkit-filter 0.7s;}'
             + '#sliderControl{padding:0;text-align:center;z-index:100;height:22px;width:100%;position:absolute;left:0;bottom:0;background:rgba(0,0,0,0.4)}'
             + '.sliderBull{transition:color 0.2s;cursor:pointer;highlight:none;color:#aaa;padding-left:4px;padding-right:4px;margin:0;text-align:center;display:inline;font-size:37px;position:relative;top:-16px;}'
             + '.sliderArr{transition:background 0.2s;z-index:100;position:absolute;top:32vh;bottom:48vh;width:50px;height:50px;padding:8px;background:rgba(0,0,0,0.05);border-radius:0px 5px 5px 0px;cursor:pointer;}'
@@ -37,12 +37,16 @@
             justChanged = false;
 
         $("#sl1").css("z-index", "1");
+        $("#sl1").css("-webkit-filter", "blur(0px)");
+        $("#sl1").css("filter", "blur(0px)");
         $("#sb1").css("color", "#fff");
 
         $("#sliderLeft").click(function () {
             $(".sliderBox").css("z-index", "0");
             $(".sliderBox").css("left", "0");
             $(".sliderBull").css("color", "#aaa");
+            $(".sliderBox").css("filter", "blur(8px)");
+            $(".sliderBox").css("-webkit-filter", "blur(8px)");
 
             slideNum--;
             if (slideNum == 0) {
@@ -51,17 +55,16 @@
 
             if (slideNum == numSlides) {
                 $("#sl1").css("z-index", "1");
-                $("#sl" + numSlides).css("z-index", "2");
-                $("#sl" + numSlides).css("left", "-100%");
-                $("#sl" + numSlides).animate({ left: "0" }, 500);
-                $("#sb" + numSlides).css("color", "#fff");
             } else {
                 $("#sl" + (slideNum + 1)).css("z-index", "1");
-                $("#sl" + slideNum).css("z-index", "2");
-                $("#sl" + slideNum).css("left", "-100%" );
-                $("#sl" + slideNum).animate({ left: "0" }, 500);
-                $("#sb" + slideNum).css("color", "#fff");
             };
+            $("#sl" + slideNum).css("z-index", "2");
+            $("#sl" + slideNum).css("filter", "blur(0px)");
+            $("#sl" + slideNum).css("-webkit-filter", "blur(0px)");
+            $("#sl" + slideNum).css("left", "-100%");
+            $("#sl" + slideNum).animate({ left: "0" }, 500);
+            $("#sb" + slideNum).css("color", "#fff");
+
             justChanged = true;
             window.setTimeout(function () {
                 justChanged = false;
@@ -71,6 +74,8 @@
             $(".sliderBox").css("z-index", "0");
             $(".sliderBox").css("left", "0");
             $(".sliderBull").css("color", "#aaa");
+            $(".sliderBox").css("filter", "blur(8px)");
+            $(".sliderBox").css("-webkit-filter", "blur(8px)");
 
             if (slideNum == numSlides) {
                 slideNum = 0;
@@ -79,10 +84,14 @@
 
             if (slideNum == 1) {
                 $("#sl" + numSlides).css("z-index", "2");
+                $("#sl" + numSlides).css("filter", "blur(0px)");
+                $("#sl" + numSlides).css("-webkit-filter", "blur(0px)");
                 $("#sl1").css("z-index", "1");
                 $("#sl" + numSlides).animate({ left: "-100%" }, 500);
                 $("#sb1").css("color", "#fff");
             } else {
+                $("#sl" + (slideNum - 1)).css("filter", "blur(0px)");
+                $("#sl" + (slideNum - 1)).css("-webkit-filter", "blur(0px)");
                 $("#sl" + (slideNum - 1)).css("z-index", "2");
                 $("#sl" + slideNum).css("z-index", "1");
                 $("#sl" + (slideNum - 1)).animate({ left: "-100%" }, 500);
@@ -100,6 +109,8 @@
                 $(".sliderBull").css("color", "#aaa");
                 $($(this)).css("color", "#fff");
                 $(".sliderBox").css("left", "0");
+                $(".sliderBox").css("filter", "blur(8px)");
+                $(".sliderBox").css("-webkit-filter", "blur(8px)");
 
                 for (var i = 1; i <= numSlides; i++) {
                     if ($("#sl" + i).css("z-index") == "1") {
@@ -110,7 +121,9 @@
 
                 $(".sliderBox").css("z-index", "0");
                 currSl.css("z-index", "2");
-                $("#sl" + bullNum).css("z-index", "1");
+                $("#sl" + bullNum).css("z-index", "2");
+                $("#sl" + bullNum).css("filter", "blur(0px)");
+                $("#sl" + bullNum).css("-webkit-filter", "blur(0px)");
                 currSl.animate({ left: "-100%" }, 500);
 
                 if (bullNum == numSlides) {
@@ -136,15 +149,21 @@
 
                 $(".sliderBox").css("z-index", "0");
                 $(".sliderBox").css("left", "0");
+                $(".sliderBox").css("filter", "blur(8px)");
+                $(".sliderBox").css("-webkit-filter", "blur(8px)");
                 $(".sliderBull").css("color", "#aaa");
 
                 if (slideNum == 1) {
                     $("#sl" + numSlides).css("z-index", "2");
                     $("#sl1").css("z-index", "1");
+                    $("#sl1").css("filter", "blur(0px)");
+                    $("#sl1").css("-webkit-filter", "blur(0px)");
                     $("#sl" + numSlides).animate({ left: "-100%" }, 500);
                     $("#sb1").css("color", "#fff");
                 } else {
                     $("#sl" + (slideNum - 1)).css("z-index", "2");
+                    $("#sl" + slideNum).css("filter", "blur(0px)");
+                    $("#sl" + slideNum).css("-webkit-filter", "blur(0px)");
                     $("#sl" + slideNum).css("z-index", "1");
                     $("#sl" + (slideNum - 1)).animate({ left: "-100%" }, 500);
                     $("#sb" + slideNum).css("color", "#fff");
